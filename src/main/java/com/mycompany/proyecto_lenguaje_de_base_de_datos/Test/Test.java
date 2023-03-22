638660d1d2c965088518b2b784871e25a22bcb0b
Jonathan Anchia <jonhanchia2@gmail.com>
2023-03-22 12:39:26 -0600

    datos test

---
+++ src/main/java/com/mycompany/proyecto_lenguaje_de_base_de_datos/Test/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto_lenguaje_de_base_de_datos.Test;
+
+/**
+ *
+ * @author jonha
+ */
+public class Test {
+    
+}
